46cb1cc179246f2722113207009e7859d5f5e3ca
subhanshu jha <subhanshujha001@gmail.com>
2024-08-04 10:31:26 +0530

    added persistant state

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import store,{persistor} from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode> 
-<Provider store={store}> 
+<Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+
 <App />
+</PersistGate>
 </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,26 @@
 // src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './inputSlice'
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Default storage for local storage
+import authReducer from './inputSlice'; // Import your existing authReducer
+
+// Create a persist config for Redux Persist
+const persistConfig = {
+  key: 'root',
+  storage, // Use localStorage by default
+};
+
+// Create a persisted reducer
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+// Configure the store with the persisted reducer
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedReducer, // Use the persisted reducer here
   },
 });
+
+// Create a persistor for Redux Persist
+export const persistor = persistStore(store);
 
 export default store;
